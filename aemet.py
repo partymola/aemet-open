1946bf9567ee11a38b4875a790ecf20b57751f93
Volker Kerkhoff <volker@kerkhoff.es>
2017-06-21 14:08:41 +0200

    Trying stuff, still broken...

--- aemet.py
+++ aemet.py
@@ -2,7 +2,7 @@
 # Spain meteo Agecy AEMET has started an OpenData project...
 # Before I was using the CSV files available, but, well, clumsy stuff
 
-import httplib, urllib, ssl, json, requests
+import httplib, urllib, ssl, json, requests, time
 
 # Load the API key from file api.key
 # Please note the API key has to be obtained at https://opendata.aemet.es/centrodedescargas/altaUsuario
@@ -19,8 +19,9 @@ header = 'api_key: ' + apikey
 
 
 allStationsUri = "/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones"
+station24hUri = "/opendata/api/observacion/convencional/datos/estacion/"
 
-uri= allStationsUri + "?api_key=" + apikey
+uri = allStationsUri + "?api_key=" + apikey
 
 #print uri
 
@@ -38,11 +39,46 @@ stations = requests.get(uriDatos, verify=False)
 stationsDict = stations.json()
 
 #debug: Print each station data as one line
+a = 0
 for p in stationsDict:
     # print p
+    a = a+1
+    print a,
     for key, value in p.items():
          print(key + ": " + value),
 
-    print '====================' #separator
+    # print '====================' #separator
+    print
 
+# get data for last 24h for each station
 
+for station in stationsDict:
+    requestUri = station24hUri + station['indicativo'] + "?api_key=" + apikey
+    stationData = httplib.HTTPSConnection("opendata.aemet.es")
+    stationData.request("GET", requestUri)
+    response = stationData.getresponse()
+    data = response.read()
+    # print data
+    predata = json.loads(data)
+
+    uriDatos = predata.get("datos")
+    uriMetadatos = predata.get("metadatos")
+
+    data24h = requests.get(uriDatos, verify=False)
+    # data24hDict = data24h.json()
+
+    for p in data24h:
+        # print p
+        a = a + 1
+        # print station['indicativo'],
+        print p
+        #try:
+        # for line in p.items():
+            # singleLine = line.json()
+            # print line,
+        #except :
+            #print "WRONG DATA"
+
+        # print '====================' #separator
+        #print
+    time.sleep(5)
