4b5b5a69679a8987db25326959be2354e6383b61
Volker Kerkhoff <volker@spainip.es>
2017-06-28 15:57:57 +0200

    Final fixes

--- aemet.py
+++ aemet.py
@@ -1,11 +1,32 @@
+#!/usr/bin/python
+# coding=UTF-8
+
 # AEMET Opendata parser
 # Spain meteo Agecy AEMET has started an OpenData project...
 # Before I was using the CSV files available, but, well, clumsy stuff
 
+# There is no full list of the Station IDs by province, so we build one from data
+# available in the dropdowns on https://opendata.aemet.es/centrodedescargas/productosAEMET?
+# Provinces are in https://opendata.aemet.es/centrodedescargas/xml/provincias.xml
+# Then for each province there is a file https://opendata.aemet.es/centrodedescargas/xml/udat/udat[ID].xml
+
 import httplib, urllib, ssl, json, requests, time
+import sys
+from elasticsearch import Elasticsearch
+import xml.etree.ElementTree
+
+# define our Elasticsearch host. Hardcoded, TO DO: Put into config file
+
+es = Elasticsearch(['localhost'],
+    port=9200,
+    use_ssl=False)
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Load the API key from file api.key
 # Please note the API key has to be obtained at https://opendata.aemet.es/centrodedescargas/altaUsuario
+# DO NOT USE my API Key, please, get your own, thanks
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -13,41 +34,96 @@ with open ("api.key", "r") as conffile:
     conf=conffile.readlines()
 
 apikey=conf[0]
-#print apikey
 
-allStations = "/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones"
-station24h = "/opendata/api/observacion/convencional/datos/estacion/"
-allLast24 = "https://opendata.aemet.es/opendata/api/observacion/convencional/todas"
+# Go get the .xml files to create the dictionary with all stations and their provinces
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+provinciasUri = '/centrodedescargas/xml/provincias.xml'
+
+conn = httplib.HTTPSConnection("opendata.aemet.es")
+conn.request("GET",provinciasUri)
+response = conn.getresponse()
+data = response.read()
+
+# Creating list for the provinces
+ssl._create_default_https_context = ssl._create_unverified_context
+
+provinciasUri = '/centrodedescargas/xml/provincias.xml'
+
+conn = httplib.HTTPSConnection("opendata.aemet.es")
+conn.request("GET",provinciasUri)
+response = conn.getresponse()
+data = response.read()
+
+# Creating list for the provinces
+provincias = xml.etree.ElementTree.fromstring(data)
+ids = []
+provs = []
+iter = provincias.getiterator('ID')
+for elem in iter:
+    elementName = elem.tag
+    elementText = elem.text
+    # print elem.tag, elem.text
+    ids.append(elementText)
+
+iter = provincias.getiterator('NOMBRE')
+for elem in iter:
+    elementName = elem.tag
+    elementText = elem.text
+    # print elem.tag, elem.text
+    provs.append(elementText)
+
+provdict = dict(zip(ids,provs))
+
+a = 0
+
+idemas = []
+thisProv = []
+
+for key in provdict:
+    # we build the URL as above
+    stationsUri = '/centrodedescargas/xml/udat/udat' + key + '.xml'
+    conn = httplib.HTTPSConnection("opendata.aemet.es")
+    conn.request("GET", stationsUri)
+    response = conn.getresponse()
+    data = response.read()
+    # data = data.split("\n",2)[1:]
+    stations = xml.etree.ElementTree.fromstring(data)
+    iter = stations.getiterator('ID')
+    currentProv = provdict[key]
+    # print currentProv
+    for elem in iter:
+        elementName = elem.tag
+        elementText = elem.text
+        # print elem.tag, elem.text
+        idemas.append(elementText)
+        #print elementText
+        thisProv.append(currentProv)
+
+# There are still some stations missing in the .xmls so I am manually adding them here
+
+missingIdemas = ['1025A', '1109X', '5107D', '7012C', '8005X', '8416X', '9001S', '9562X', '9718X', '9783B', 'C619Y', 'C839X', 'C919K','4492F']
+
+for i in missingIdemas:
+    idemas.append(i)
+
+missingProvs = ['Gipuzkoa', 'Santander', 'Granada', 'Murcia', 'València/Valencia', 'València/Valencia', 'Santander', 'Castelló/Castellón', 'Barcelona', 'Huesca', 'Las Palmas', 'Las Palmas', 'Santa Cruz de Tenerife', 'Badajoz']
+
+for i in missingProvs:
+    thisProv.append(i)
+
+stationsdict = dict(zip(idemas,thisProv))
+
+# This is where all the magic of getting and parsing all data reported in the last 24 hours from all stations happens
+
+allLast24 = "/opendata/api/observacion/convencional/todas"
 
 header = 'api_key: ' + apikey
 
-def getUri(aemetData):
-    try:
-        conn = httplib.HTTPSConnection("opendata.aemet.es")
-        conn.request("GET", aemetData)
-        response = conn.getresponse()
-        data = response.read()
-        print data
-        predata = json.loads(data)
+uri = allLast24 + "?api_key=" + apikey
 
-        uriDatos = predata.get("datos")
-        uriMetadatos = predata.get("metadatos")
-
-    except:
-        print "Ooops"
-    return
-
-
-# header = 'api_key: ' + apikey
-
-
-allStationsUri = "/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones"
-station24hUri = "/opendata/api/observacion/convencional/datos/estacion/"
-
-
-uri = allStationsUri + "?api_key=" + apikey
-
-#print uri
+# Funny way of doing stuff, we first need to get an URL which then points to the actual data
 
 conn = httplib.HTTPSConnection("opendata.aemet.es")
 conn.request("GET",uri)
@@ -59,47 +135,75 @@ predata = json.loads(data)
 uriDatos = predata.get("datos")
 uriMetadatos = predata.get("metadatos")
 
-stations = requests.get(uriDatos, verify=False)
-stationsDict = stations.json()
+# Here we get the actual data
 
-#debug: Print each station data as one line
-a = 0
-for p in stationsDict:
-    # print p
-    a = a+1
-    print a,
-    for key, value in p.items():
-         print(key + ": " + value),
+conn = httplib.HTTPSConnection("opendata.aemet.es")
+conn.request("GET", uriDatos)
+response = conn.getresponse()
+data = response.read()
+data = data.decode('iso-8859-1').encode('utf-8')
+p = json.loads(data, 'utf-8')
 
-    # print '====================' #separator
-    print
+# iterate through the individual data lines
 
-# get data for last 24h for each station
+counter = 0
+creations = 0
+updates = 0
+failures = 0
 
-for station in stationsDict:
-    requestUri = station24hUri + station['indicativo'] + "?api_key=" + apikey
-    conn = httplib.HTTPSConnection("opendata.aemet.es")
-    conn.request("GET", requestUri)
-    response = conn.getresponse()
-    data = response.read()
-    predata = json.loads(data)
+print "Now indexing data into ElasticSearch",
 
-    uriDatos24h = predata.get("datos")
-    station24h = requests.get(uriDatos24h, verify=False)
-    data24h = station24h.json()
+for i in p:
+    counter = counter + 1 # Just a variable to show the final number of records
+    # I use a trick, so that when data from a station comes late, it will update with the latter
+    # by calculating the ES ID with the Date and time (fint) and the Station ID (idema)
+    # so... calcutating ElasticSearch ID
+    datetime = i['fint'].encode('ascii', 'ignore')
+    idema = i['idema'].encode('ascii', 'ignore')
+    rawId = datetime + idema
+    esId = filter(str.isalnum, rawId)
+    # I want my data indexed by month, so we extract the month from datetime. Change the index name to your likings
+    indexName = 'aemet-open-' + datetime[0:4] + '.' + datetime[5:7]
+    # For all this to work, we need to define an index template in Elasticsearch.
+    # See the file index_template.json for this. Of course, you need to adapt it to your index name
 
-    print type(predata)
+    # We also generate the geolocation field if you want to have nice maps...
 
-    #for p in predata:
-        # print p
-        # a = a + 1
-        # for a in p:
-            #print a,
-            #singleLine = line.json()
-            #print line
-        #except :
-            #print "WRONG DATA"
+    lat = str(i['lat']).encode('ascii', 'ignore')
+    lon = str(i['lon']).encode('ascii', 'ignore')
+    geolocation = lat + ',' + lon
 
-        # print '====================' #separator
-        #print
-    time.sleep(10)
+    # We add the @timestamp and geolocation fields into the python list that will be indexed
+    i['geolocation'] = geolocation
+    i['@timestamp'] = datetime
+
+    # We add the province from the stations dictionary generated above
+    try:
+        estaProv = stationsdict[idema]
+        # print idema, estaProv
+        i['provincia'] = estaProv
+    except:
+        # Warn if there is no province data for a given station so wwe can correct it
+        print 'No Province info for ' + idema
+    try:
+        # we index the result in Elasticsearch
+        res = es.index(index=indexName, doc_type='aemet', id=esId, body=i)
+        # and stats...
+        if res['result'] == 'created':
+            creations = creations + 1
+        elif res['result'] == 'updated':
+            updates = updates + 1
+        # Print progress dots every 50 ops
+        if counter%50 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+    except:
+        # Something has failed so we need to take a closer look
+        # print 'F',
+        failures = failures + 1
+
+# stats, stats...
+print
+print 'A total of ' + str(counter) + ' lines of data where received. ' + str(creations) + ' documents were created, ' + str(updates) + ' were updated and ' + str(failures) + ' ElasticSearch operations failed.'
+
+
